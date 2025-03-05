77d63217c14875ed30267b271f5d8fba53d68339
Freja Fischer <frej657e@stud.kea.dk>
2025-03-05 15:41:40 +0100

    styled landingpage

--- vibe-client/src/pages/Landing.tsx
+++ vibe-client/src/pages/Landing.tsx
@@ -1,41 +1,22 @@
+import VinylsGraphic from "../components/VinylsGraphic";
+
 const Landing = () => {
   return (
-    <main>
-      <h1 className="text-6xl font-light font-display flex flex-col">
-        Welcome to <span className="text-8xl font-bold">vibeVault</span>
-      </h1>
-      <p className="text-2xl">
-        <a href="/login" className="underline">
-          Login
-        </a>{" "}
-        or{" "}
-        <a href="/signup" className="underline">
-          Signup
-        </a>
-      </p>
-      <div className="relative w-[550px] h-[450px]">
-        <svg width="310" height="310" viewBox="0 0 310 310" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-primary absolute bottom-0 right-0">
-          <circle cx="155" cy="155" r="150" stroke="currentColor" stroke-width="5" />
-          <circle cx="155" cy="155" r="135" fill="currentColor" />
-          <circle cx="155" cy="155" r="120" fill="#fff" stroke="currentColor" stroke-width="3" />
-          <circle cx="155" cy="155" r="105" fill="currentColor" />
-          <circle cx="155" cy="155" r="90" fill="#fff" stroke="currentColor" stroke-width="3" />
-          <circle cx="155" cy="155" r="75" fill="currentColor" />
-          <circle cx="155" cy="155" r="60" fill="#fff" stroke="currentColor" stroke-width="3" />
-          <circle cx="155" cy="155" r="30" fill="currentColor" />
-          <circle cx="155" cy="155" r="5" fill="#fff" />
-        </svg>
-        <svg width="310" height="310" viewBox="0 0 310 310" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-black">
-          <circle cx="155" cy="155" r="150" stroke="currentColor" stroke-width="5" />
-          <circle cx="155" cy="155" r="135" fill="currentColor" />
-          <circle cx="155" cy="155" r="120" fill="#fff" stroke="currentColor" stroke-width="3" />
-          <circle cx="155" cy="155" r="105" fill="currentColor" />
-          <circle cx="155" cy="155" r="90" fill="#fff" stroke="currentColor" stroke-width="3" />
-          <circle cx="155" cy="155" r="75" fill="currentColor" />
-          <circle cx="155" cy="155" r="60" fill="#fff" stroke="currentColor" stroke-width="3" />
-          <circle cx="155" cy="155" r="30" fill="currentColor" />
-          <circle cx="155" cy="155" r="5" fill="#fff" />
-        </svg>
+    <main className="mx-5 my-6 md:my-10 flex justify-center">
+      <div className="max-w-6xl flex flex-col gap-6 xs:grid xs:grid-cols-2 xs:grid-rows-2 xs:gap-1 sm:grid-cols-7 sm:gap-4 md:mx-12 lg:grid-cols-12 lg:gap-5">
+        <h1 className="text-4xl font-light font-display flex flex-col h-fit gap-1 xs:col-start-1 xs:row-1 xs:self-end sm:col-span-3 lg:text-6xl lg:col-start-1 lg:col-span-5">
+          Welcome to <span className="text-5xl lg:text-8xl font-bold">vibeVault</span>
+        </h1>
+        <p className="text-xl xs:col-start-1 xs:row-start-2 sm:col-span-3 md:text-2xl lg:col-start-1">
+          <a href="/login" className="underline">
+            Login
+          </a>{" "}
+          or{" "}
+          <a href="/signup" className="underline">
+            Signup
+          </a>
+        </p>
+        <VinylsGraphic styling="self-center xs:col-start-2 xs:row-span-2 sm:col-start-4 lg:col-start-7 lg:row-span-2" />
       </div>
     </main>
   );
