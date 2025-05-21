d5aa46e89551020b596ee84daa2584dc8b4fd8dc
Freja Fischer <frej657e@stud.kea.dk>
2025-05-21 13:15:46 +0200

    test if server deploy hook is triggered

--- vibe-server/startup/dbConnection.ts
+++ vibe-server/startup/dbConnection.ts
@@ -2,7 +2,7 @@ import { AppDataSource } from "./data-source";
 
 const dbConnectPostgress = async () => {
   try {
-    await AppDataSource.initialize(); // Connect to postgres db
+    await AppDataSource.initialize(); // Connect to postgres database
     console.log("Connected to Postgres database");
   } catch (error) {
     console.log("Error connecting to Postgres database", error);
