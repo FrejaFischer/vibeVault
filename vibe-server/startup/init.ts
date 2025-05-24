6a7058f59834f25893ad3db41b52fa691f7c0555
Freja Fischer <frej657e@stud.kea.dk>
2025-05-24 14:01:43 +0200

    installed cookie parser and setup API to be able to parse cookies

--- vibe-server/startup/init.ts
+++ vibe-server/startup/init.ts
@@ -2,9 +2,11 @@ import { Application, json } from "express";
 import cors from "cors";
 import dbConnectPostgress from "./dbConnection";
 import setupRouters from "./setupRouters";
+import cookieParser from "cookie-parser";
 
 const init = (app: Application) => {
   app.use(cors()); // Enable CORS for all requests to the server
+  app.use(cookieParser()); // Enable a cookie parser to get cookies in request
   app.use(json()); // Enable JSON parsing for all requests to the server
 
   dbConnectPostgress(); // Connect to the Postgres database
