7825c69dd45d02c7a79ee160b0a1cf9156ea937a
Freja Fischer <frej657e@stud.kea.dk>
2025-06-04 09:50:24 +0200

    authenticate if user is logged in or not, and handle redirect if so

--- vibe-client/src/pages/AuthLayout.tsx
+++ vibe-client/src/pages/AuthLayout.tsx
@@ -1,8 +1,30 @@
-import { Outlet } from "react-router";
 import AuthNav from "../components/AuthNav";
+import { Outlet, useNavigate } from "react-router";
 import Header from "../components/Header";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const AuthLayout = () => {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  // Check if user is logged in (has token in cookie) - if true redirect to entries page
+  useEffect(() => {
+    if (!auth?.loading && auth?.isAuthenticated) {
+      navigate("/entries", { replace: true });
+    }
+  }, [auth, navigate]);
+
+  // While checking the authentication, show this:
+  if (auth?.loading) {
+    return <p>Loading...</p>;
+  }
+
+  // Fallback while redirecting (so the page is not viewed before redirecting works)
+  if (auth?.isAuthenticated) {
+    return null;
+  }
+
   return (
     <>
       <Header>
