5af44c5cc26c69c5fc393a2dcf690e985052091b
Freja Fischer <frej657e@stud.kea.dk>
2025-06-05 11:04:34 +0200

    made artists routed protected

--- vibe-server/routes/artistRouter.ts
+++ vibe-server/routes/artistRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getArtists, getArtistAlbums } from "../controllers/artistController";
+import { verifyToken } from "../middleware/verifyToken";
 
 const artistRouter = Router();
 
-artistRouter.get("/", getArtists); // GET route, for getting all albums, handles optional search query
-artistRouter.get("/:artist_id/albums", getArtistAlbums); // GET route, for getting all albums by a specific artist
+artistRouter.get("/", verifyToken, getArtists); // GET route, for getting all albums, handles optional search query
+artistRouter.get("/:artist_id/albums", verifyToken, getArtistAlbums); // GET route, for getting all albums by a specific artist
 
 export default artistRouter;
