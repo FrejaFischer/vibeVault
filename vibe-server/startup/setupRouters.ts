7cae197af7a27da35989f6e9e141be64a3d94e91
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2025-06-11 12:16:53 +0200

    redundant routes deleted

--- vibe-server/startup/setupRouters.ts
+++ vibe-server/startup/setupRouters.ts
@@ -10,13 +10,10 @@ import authRouter from "../routes/authRouter";
 
 // Setup all our routes / endpoints
 const setupRouters = (app: Application) => {
-  // app.use("/test", testRouter); // Test endpoint for get and post test user (dont work anymore)
   app.use("/artists", artistRouter); // Test endpoint for getting artists
   app.use("/albums", albumRouter); // Test endpoint for getting albums
   app.use("/tracks", trackRouter); // Test endpoint for getting songs
-  app.use("/users/:user_id/entries", entryRouter); // Endpoint for getting users entries, :user_id is a route param
   app.use("/entries", entryRouter); // Endpoint for getting all entries
-  //app.use("/users/:user_id/entries/:entry_id", entryRouter); // Endpoint for getting a specific entry
   app.use("/users", userRouter); // Endpoint for users (insert + get by id)
   app.use("/login", loginRouter); // Endpoint for login
   app.use("/auth/check", authRouter); // Endpoint for checking authentication in server
