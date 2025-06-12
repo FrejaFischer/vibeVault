e15f74105e23e7ffc1dc6fe4b86f984b336bf3f3
Freja Fischer <frej657e@stud.kea.dk>
2025-06-12 13:24:32 +0200

    implemented styling in signup page + components

--- vibe-client/src/components/InputField.tsx
+++ vibe-client/src/components/InputField.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 const InputField = ({ id, name, type, value, onChange }: Props) => {
-  return <input type={type} id={id} name={name} value={value} onChange={onChange} className="border-solid border-black border rounded-lg p-2 mt-2 md:p-3 md:text-xl lg:text-2xl focus:outline-primary focus:outline-2" />;
+  return <input type={type} id={id} name={name} value={value} onChange={onChange} className="bg-neutral-brand-150 border-solid border-black border rounded-lg p-2 mt-2 md:p-3 md:text-xl lg:text-2xl focus:outline-positive-brand-200 focus:outline-2" />;
 };
 
 export default InputField;
--- vibe-client/src/pages/SignupPage.tsx
+++ vibe-client/src/pages/SignupPage.tsx
@@ -1,12 +1,13 @@
 import SignupForm from "../components/SignupForm";
+import VinylsGraphic from "../components/VinylsGraphic";
 
 const SignupPage = () => {
   return (
     <>
       <title>vibeVault - Signup</title>
-      <main>
-        <h1>Signup</h1>
+      <main className="md:flex md:gap-10 md:mx-20 md:justify-center">
         <SignupForm />
+        <VinylsGraphic styling=" hidden md:block md:self-center" />
       </main>
     </>
   );
