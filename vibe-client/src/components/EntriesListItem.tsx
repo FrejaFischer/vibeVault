053a7039e4c23877bf2c98b11dbcd5b7f0c41651
Freja Fischer <frej657e@stud.kea.dk>
2025-06-17 11:30:38 +0200

    handle placeholders, if users image file dont exist or is valid

--- vibe-client/src/components/EntriesListItem.tsx
+++ vibe-client/src/components/EntriesListItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Entry } from "../types/entry";
 import { NavLink } from "react-router";
+import { isSafeImageFilename } from "@/validation/imagePathValidation";
+import { getRandomFallbackImage } from "@/utils/findPlaceholderImg";
 
 interface EntryListItemProps {
   entry: Entry;
@@ -21,8 +23,17 @@ const EntriesListItem: React.FC<EntryListItemProps> = ({ entry }) => {
     <li key={entry.entry_id}>
       <NavLink to={`/entries/${entry.entry_id}`}>
         <article className="flex gap-7 items-center w-full">
-          <div className="bg-neutral-brand-150 w-15 rounded-lg aspect-square md:w-19 md:h-19">
-            <img src={entry.cover_image} alt={entry.title} className="w-full object-cover aspect-square" />
+          <div className="bg-blue-brand-100 w-15 rounded-lg aspect-square md:w-19 md:h-19">
+            <img
+              src={isSafeImageFilename(entry.cover_image) ? entry.cover_image : "fallback1.jpeg"}
+              alt={entry.title}
+              onError={(e) => {
+                // If the image is not found, fallback to placeholder image
+                const placeholder = getRandomFallbackImage();
+                e.currentTarget.src = placeholder;
+              }}
+              className="w-full object-cover aspect-square"
+            />
           </div>
           <div className="flex flex-col md:items-center w-full justify-between md:flex-row">
             <h2 className="text-xl md:text-2xl">{entry.title}</h2>
